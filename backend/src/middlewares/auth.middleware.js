8bbe8afc0a7bd36fbd8ca427b63c519474d236d2
yuvan <1095yuvangrover@gmail.com>
2024-09-16 17:41:28 +0530

    log out dne

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
-        const token =   await req.cookies?.accesstoken || req.header("Authorization")?.replace("Bearer ", "");
+        const token =   await req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
         if (!token) {
             throw new ApiError(401, "Unauthorized request");
         }
